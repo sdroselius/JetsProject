24d561d62c9cc2fdd74096c394063ed5a1c4eab7
Rob Roselius <roselius@skilldistillery.com>
2024-11-08 14:40:32 -0700

    Stub application and AirField classes

---
+++ src/com/skilldistillery/jets/app/AirField.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.jets.app;
+
+import java.util.List;
+
+import com.skilldistillery.jets.entities.Jet;
+
+public class AirField {
+	
+	private List<Jet> fleet;
+	// NO other fields
+
+}
--- src/com/skilldistillery/jets/app/JetsApplication.java
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -1,10 +1,32 @@
 package com.skilldistillery.jets.app;
 
+import java.util.Scanner;
+
 public class JetsApplication {
+	
+	private AirField airField = new AirField();
+	private static Scanner scanner;
+	
+	// NO other fields!  NO list of jets!
  
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		scanner = new Scanner(System.in);
+		JetsApplication app = new JetsApplication();
+		app.launch();
+		
+		scanner.close();
+	}
 
+	private void launch() {
+		// TODO Loop:
+		// displayUserMenu()
+		// get choice from scanner
+		// act on choice
+		
+	}
+	
+	private void displayUserMenu() {
+		// TODO sysouts for menu
 	}
 
 }
